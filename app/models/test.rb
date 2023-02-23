d34599ba37b862c77d59ba793d029db8201ab2bf
Lesiantus <sashkabonan@gmail.com>
2023-02-23 18:56:57 +0700

    created seed for db, method for user and test models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.sort_by_categories(category)
+    joins(:categories).where(categories: { title: category }).order('tests.title DESC').pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(name: "Example User", email: "example@mail.org", admin: true)
+
+categories = Category.create!([
+  { title: 'Ruby' },
+  { title: 'Ruby on Rails' }
+])
+
+tests = Test.create!([
+  { title: 'Arrays', level: 0, category_id: categories[0].id, author_id: user.id },
+  {title: 'Hashes', level: 1, category_id: categories[0].id, author_id: user.id },
+  {title: 'Controller', level: 2, category_id: categories[1].id, author_id: user.id }
+])
+
+questions = Question.create!([
+  { body: 'What method removes all nil elements from the array?', test_id: tests[0].id },
+  { body: 'What method returns true if any element satisfies a given criterion?', test_id: tests[1].id},
+  { body: 'From what class inherits all new generated controllers?', test_id: tests[2].id }
+])
+
+answers = Answer.create!([
+{ body: 'compact', correct: true, question_id: questions[0].id},
+{ body: 'any?', correct: true, question_id: questions[1].id },
+{ body: 'ActionController', correct: true, question_id: questions[2].id}
+])

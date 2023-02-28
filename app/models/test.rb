49d699ae3ab7b3eb2f1b94f6348e534012618f6f
Lesiantus <sashkabonan@gmail.com>
2023-02-28 22:13:14 +0700

    Added associations fo models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results
+
   def self.sort_by_categories(name)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: name })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :tests
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results
+
   def tests_by_level(level)
     Test.joins('JOIN results ON results.test_id = tests.id')
         .where(results: { user_id: id }, tests: { level: level })

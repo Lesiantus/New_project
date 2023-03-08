0e5284cc6c4859e2fc938eb2ae094443ee1ee61b
Lesiantus <sashkabonan@gmail.com>
2023-03-08 21:29:23 +0700

    suggested remarks changes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,29 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create new destroy show]
-  before_action :find_question, only: %i[show destroy create]
+  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
 
-  rescue_from NoMethodError, with: :rescue_with_question_not_found
-  def new; end
-
-  def show
-    #render inline: 'Тест <%=@question.test_id %>, Вопрос <%=@question.id %>: <%=@question.body %>'
-  end
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     render inline: 'Все вопросы к тесту: <br> <%= @test.questions.pluck(:body) %>'
   end
 
-  def destroy
-    @question.destroy
-    redirect_to :back
-  end
+  def show; end
+
+  def new; end
 
   def create
-    question = @test.questions.create(question_params)
-    if question.save
-      redirect_to question
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to @question
     else
-      render plain: question.inspect
+      render plain: @question.inspect
     end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to question_url
   end
 
   private
@@ -34,14 +33,14 @@ class QuestionsController < ApplicationController
   end
 
   def find_test
-    @test = Test.find_by(id: params[:test_id])
+    @test = Test.find(params[:test_id])
   end
 
   def find_question
-    @question = Question.find_by(id: params[:id])
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
-    render plain: 'Такого вопроса пока не существует!'
+    render plain: '404 Не найден!'
   end
 end

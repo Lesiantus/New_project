ee89fc644bfde947050c7e698db846a4564502fa
Lesiantus <sashkabonan@gmail.com>
2023-03-13 23:43:22 +0700

    first commit for views

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,58 @@
+class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update destroy]
+  before_action :find_questions, only: %i[show]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def find_questions
+    @questions = @test.questions
+  end
+
+  def rescue_with_test_not_found
+    render plain: '404 Не найден!'
+  end
+end

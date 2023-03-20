c858bd3b7dd3559d321dcc78765d652146e901c6
Lesiantus <sashkabonan@gmail.com>
2023-03-20 23:10:42 +0700

    Added callback

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,6 +4,7 @@ class Result < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_first_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -13,8 +14,6 @@ class Result < ApplicationRecord
     if correct_answer?(answer_ids)
       self.correct_question += 1
     end
-
-    self.current_question = next_question
     save!
   end
 
@@ -32,7 +31,7 @@ class Result < ApplicationRecord
     current_question.answers.correct
   end
 
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+  def before_validation_set_first_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
